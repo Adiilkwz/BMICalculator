34af862cbe3b3eccf8322899bb98292f39c757f0
adramazan <240692@astanait.edu.kz>
2025-12-09 13:18:39 +0500

    feat: implement home route and form handling middleware

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/calculate-bmi', (req, res) => {
+    console.log(req.body);
+    res.send("Data received!");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
